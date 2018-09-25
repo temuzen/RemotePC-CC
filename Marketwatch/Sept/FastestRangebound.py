a498a8abfe65b2ae680355e933d0e079f85042f8
Yash Panchal <34062562+temuzen@users.noreply.github.com>
2018-09-25 14:19:13 -0700

    Finally Fastest Script

---
+++ Marketwatch/Sept/FastestRangebound.py
@@ -0,0 +1,35 @@
+#Nifty50MW.xlsx
+#Nifty50MW.xlsx
+#Optionswatch.xlsx
+
+from xlwings import Book, Sheet, Range, Chart
+import xlwings as xw
+import time
+App = xw.App()
+
+sht = xw.Book(r'C:\Users\admin\Documents\GitHub\RemotePC-CC\Marketwatch\Sept\watchlist.xlsx').sheets[0]
+wrt = xw.books('Book1').sheets[0]
+i = 1
+	
+
+while(1):    
+    s1 = time.time()
+    String1 = str(sht.range('A2:M110').value)
+    e1 = time.time()
+   # print("Time S1:"+str(e1-s1))
+    s2x = time.time()
+    s2 = time.time()
+    String2 = str(sht.range('A2:M110').value)
+    e2 = time.time()
+  #  print("Time S2:"+str(e2-s2))
+    #String1 = String1 + str(sht.range('A'+str(x)+':'+'M'+str(x)).value)
+    
+  #  print(len(String1))
+  #  print(String1)
+  #  print(s2-s2x)
+    if String1==String2:
+        continue    
+    else:
+        wrt.range('A'+str(i+1)).value = String2          
+        i=i+1
+   
